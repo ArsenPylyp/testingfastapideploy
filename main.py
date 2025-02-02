20372dd1ae46122ada45b8358360afcb8a357dd3
Arsen Pylyp <pylyparsen1@gmail.com>
2025-02-02 18:51:55 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Depends
+
+from contextlib import asynccontextmanager
+
+from database import create_tables, delete_tables
+from router import router as tasks_router
+
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    await delete_tables()
+    print("base cleaned")
+    await create_tables()
+    print("started base")
+    yield
+    print("closing")
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
+
+
+
+
+
+
+
+#if __name__ == '__main__':
+#    print_hi('PyCharm')
